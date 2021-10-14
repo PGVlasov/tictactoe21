a8bbc1ea251ed80c254ab7d8a3fac33b92bf1f51
PGVlasov <47336018+PGVlasov@users.noreply.github.com>
2021-10-14 22:08:50 +0300

    Delete keys directory

--- keys/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  MONGODB_URI:
-    "mongodb+srv://PGVlasov:pYysGR4QudCqufWH@cluster0.qjjvx.mongodb.net/tictactoe2021",
-  SESION_SECRET: "some secret value",
-  SENDGRID_API_KEY:
-    "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",
-  EMAIL_FROM: "pvlasov@yandex.ru",
-  BASE_URL: "http://localhost:3000",
-};
